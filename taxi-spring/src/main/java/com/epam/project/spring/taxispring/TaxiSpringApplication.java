a50beecafc3b3ae740037923352844d8b3c5a6da
dodo <77271382+daria-zubritska@users.noreply.github.com>
2022-02-21 19:48:01 +0200

    Bones

---
+++ taxi-spring/src/main/java/com/epam/project/spring/taxispring/TaxiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.epam.project.spring.taxispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiSpringApplication.class, args);
+	}
+
+}
---
+++ taxi-spring/src/test/java/com/epam/project/spring/taxispring/TaxiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.project.spring.taxispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
